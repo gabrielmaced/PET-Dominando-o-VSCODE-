3fe5b03daa8cd0ed618cc4ca064ed22f21156926
gabrielmaced <gabriellmacedo25@hotmail.com>
2023-10-25 22:16:59 -0300

    Minicurso

---
+++ VersaoFinal.c
@@ -0,0 +1,34 @@
+// BEM VINDOS AO MINICURSO DOMINANDO O VSCADE
+// VAMOS APRENDER HABILIDADES MAGICAS PARA MANIPULAR NOSSOS CODIGOS NO VSCADE
+// SEU AMIGO NAO VAI ENTENDER NADA VENDO VOCE PROGRAMAR no VSCADE
+// VOCE PRECISA PRINTAR A VARIAVEL i 5 VEZES NESSE CODIGO ALTERNANDO ENTRE i = 10 e i = 50
+int main(){
+    int i;
+    int x;
+    int k;
+
+    x = 10;
+    i = x;
+    printf("numero %d", i);
+    x = 50;
+    i = x;
+    printf("numero %d", i);
+    x = 10;
+    i = x;
+    printf("numero %d", i);
+    x = 50;
+    i = x;
+    printf("numero %d", i);
+    x = 10;
+    i = x;
+    printf("numero %d", i);
+
+
+    // k=0:::
+
+    for(i=0;i< 10;i++){
+    // este for NAO DEVE FAZER ABSOLUTAMENTE NADA
+    };
+}
+
+
